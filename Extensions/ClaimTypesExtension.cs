848d671b0781dad61111271b9dd8763a6b058601
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-29 11:16:22 -0300

    adding ext methods

--- Extensions/ClaimTypesExtension.cs
+++ Extensions/ClaimTypesExtension.cs
@@ -22,11 +22,24 @@ public static class ClaimTypesExtension
     {
         try
         {
-            var name = user.Claims.FirstOrDefault(x => x.Type == "Name").Value ?? "";
+            var name = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value ?? "";
             return name;
         }
         catch (Exception)
         {
+            return "";
+        }
+    }
+
+    public static string Email(this ClaimsPrincipal user)
+    {
+        try
+        {
+            var email = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email).Value ?? "";
+            return email;
+        }
+        catch (System.Exception)
+        {            
             return "";
         }
     }
